6ba2df334807aeddd2ee35c53f8364628b8e921a
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-15 19:47:05 -0700

    added login page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ViewAllPhotos from './Components/ViewAllPhotos';
 import AddPhotoEntry from './AddPhotoEntry';
 import PhotoNavbar from './Components/PhotoNavbar';
+import Login from './Components/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<AddPhotoEntry />} />
+      <Route path="/Add" element={<AddPhotoEntry />} />
       <Route path="/ViewAll" element={<ViewAllPhotos />} />
+      <Route path="/" element={<Login />} />
     </Routes>
     </BrowserRouter>
   );
